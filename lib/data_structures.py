a532162963bf5c506efbd2bef1ad20955e6327ee
wathika-eng <wathika02@gmail.com>
2024-05-31 23:19:59 +0300

    chore: Refactor data_structures.py for improved readability and functionality

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,41 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(food["name"])
+
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = [food for food in spicy_foods if food["heat_level"] > 5]
+    return spiciest_foods
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+    spiciest_foods = [food for food in spicy_foods if food["heat_level"] > 1]
+    food_heat_levels = []
+    for food in spiciest_foods:
+        heat_level = ""
+        for _ in range(food["heat_level"]):
+            heat_level += "🌶"
+        food_heat_levels.append(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}"
+        )
+    return food_heat_levels
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
 
+
 def print_spiciest_foods(spicy_foods):
     pass
 
+
 def get_average_heat_level(spicy_foods):
     pass
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
---
+++ lib/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+    # while heat <= spiciest_foods["heat_level"]:
+    #     heat += 1
+    #     print(heat)
